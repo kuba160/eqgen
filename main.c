d2fceed8507f3d41e8f03320dfba3a0cea862b54
Jakub Wasylków <kuba_160@protonmail.com>
2018-02-07 12:17:20 +0100

    Update main.c.

--- main.c
+++ main.c
@@ -1,37 +1,94 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+// The second approach to eqgen
 
-#define round(x) ((x)>=0?(long)((x)+0.5):(long)((x)-0.5))
 
+// Structure for resposiveness
 typedef struct freqresp{
 	float frequency;
 	float responsiveness;
-} freqresp;
+	float difference;
+};
 
-int compare (const void * a, const void * b)
-{
+typedef struct equalizer{
+	float frequency;
+	float value;
+};
 
-  freqresp *A = (freqresp *)a;
-  freqresp *B = (freqresp *)b;
+typedef struct foobar_eq{
+	const float freq[18] = {55.0,77.0,110.0,156.0,220.0,311.0,440.0,622.0,880.0,1200.0,1800.0,2500.0,3500.0,5000.0,7000.0,10000.0,14000.0,20000.0};
+	unsigned char value[18];
+};
 
-  return ( A->frequency - B->frequency );
-}
+typedef struct deadbeef_eq{
+	const float freq[18] = {55.0,77.0,110.0,156.0,220.0,311.0,440.0,622.0,880.0,1200.0,1800.0,2500.0,3500.0,5000.0,7000.0,10000.0,14000.0,20000.0};
+	float value[18];
+};
 
-int
-main (int argc, char * argv[]){
-	#define VALUES_MAX 1000
-	struct freqresp values[VALUES_MAX];
-	
+// Calculate best value to equalize for frequency
+// Add all values which are between these 2 values and divide them by sum
+// You will get avergave for specific frequency
+typedef struct range{
+	const float min = {20.0,66.0,98.0,137.5,192.5,270.0,375.5,531.0,751.0,1040.0,1500.0,2150.0,3000.0,4250.0,6000.0,8500.0,12000.0,17000.0};
+	const float max = {66.0,98.0,137.5,192.5,270.0,375.5,531.0,751.0,1040.0,1500.0,2150.0,3000.0,4250.0,6000.0,8500.0,12000.0,17000.0,20000.0};
+};
+
+#include <stdio.h>
+#include <libarg.h>
+
+#define FREQ_COUNT_MAX 1024
+
+int main (int argc, char * argv[]) {
+	LibARG();
+	char * filename = FreeMandatoryStringCase("Filename", "FILE");
+	int make_foobar_eq = SelectCase('f', "--foobar", "Generate foobar eq");
+	int make_deadbeef_eq = SelectCase('d', "--deadbeef", "Generate deadbeef eq");
+	LibARG();
+
 	FILE * pFile;
-	char buffer[1024];
-	pFile = fopen("Bose.csv","r");
+	pFile = fopen(filename,"r");
 	if(!pFile){
-		printf ("error opening file\n");
+		printf ("Error while opening file. %s\n",ferror(pFile));
 		exit (1);
 	}
+
+	// File loaded, read values of it
+
+	struct freqresp freq_table[FREQ_COUNT_MAX];
+	int err = ReadValues(pFile,&freq_table);
+	if (err) {
+		printf ("Error while reading values from file.\n");
+		exit (1);
+	}
+	fclose (pFile);
+
+	// We have values in freq_table[], first normalize them and then make an equalizer
+
+	Normalize (freq_table);
+
+	struct foobar_eq foobareq = GenFoobarEQ (freq_table);
+
+	printf("Foobar eq data:\n");
+
+	if(make_foobar_eq){
+		char * outfilename = MakeFileName(fiename,"feq");
+		FILE * pFileOut;
+		pFileOut = fopen(outfilename,"w");
+		if(!pFileOut){
+			printf ("Error while opening output file: %s\n",outfilename);
+			exit (1);
+		}
+		for (i = 0; i < 18; i++){
+			printf("%.0fHz: %d\n",foobareq.freq[i],foobareq.value[i]);
+			fprintf(pFileOut,"%d\n",foobareq.value[i]);
+		}
+		fclose(pFileOut);
+	}
+}
+
+int ReadValues (FILE * File, freqresp & freqtable) {
+
+	char buffer[1024];
 	int n=0,i=0;
- 	while (fgets(buffer, sizeof(buffer), pFile)) {
+ 	while (fgets(buffer, sizeof(buffer), File)) {
  		if(!strncmp(buffer,";;",2))
  			break;
 		char *pch = strtok (buffer,";");
@@ -44,7 +101,7 @@ main (int argc, char * argv[]){
 			float temp = strtof(pch,NULL);
 			if(temp == 0.0)
 				continue;
-			values[n].frequency = temp;
+			freqtable[n].frequency = temp;
 		}
 		pch = strtok (NULL, ";");
 		if (pch != NULL){
@@ -55,53 +112,49 @@ main (int argc, char * argv[]){
 			float temp = strtof(pch,NULL);
 			if(temp == 0.0)
 				continue;
-			values[n].responsiveness = temp;
+			freqtable[n].responsiveness = temp;
 		}
 		n++;
+		if (n > FREQ_COUNT_MAX) {
+			printf ("Value count overflow!\n");
+			freqtable[n] = (freqresp) {0,0};
+			return -1;
+		}
     }
-    values[n] = (freqresp) {0,0};
-	/*while(!feof(pFile)){
-		pch = strtok (str," ,.-");
-		while (pch != NULL)
-		{
-			printf ("%s\n",pch);
-		    pch = strtok (NULL, " ,.-");
+    freqtable[n] = (freqresp) {0,0};
+
+    return 0;
+}
+
+char * MakeFileName(char * inputfile, char * ending) {
+	char * outfilename = malloc(strlen(inputfile)+strlen(ending)+1);
+	strcpy(outfilename,inputfile);
+	char * dotpointer = strrchr(outfilename,'.');
+	if(!dotpointer){
+		dotpointer = strrchr(outfilename,'\0');
+		if(ending[0] == '.')
+			strcpy(dotpointer,ending);
+		else {
+			dotpointer[0] = '.';
+			strcpy(dotpointer+1,ending);
 		}
-		int err = fscanf (pFile, "%f;%f", &(values[n].frequency), &(values[n].responsiveness));
-		printf("fscanf returned %d\n",err);
-		n++;
-		if(n >= VALUES_MAX){
-			printf("values overflow");
-			exit (1);
-		}
-	}*/
-	fclose(pFile);
-	/*
-	printf ("FREQ RESP\n");
-	int n = 0, i = 0;
-	for(;;){
-		int err = scanf ("%d %d",&(values[n].frequency),&(values[n].responsiveness));
-		if (err != 2){
-			printf ("invalid values, retry?\n");
-			if ( getchar() == 'y')
-				continue;
-			else
-				break;
-		}
-		n++;
 	}
-	*/
-	qsort (values, n, sizeof(freqresp), compare);
-	printf("Read %d values\n",n);
-	/*
-	for (i = 0; i < n; i++){
-		printf("%.02f %.02f\n",values[i].frequency,values[i].responsiveness);
+	else {
+		if(ending[0] == '.')
+			strcpy(dotpointer,ending);
+		else
+			strcpy(dotpointer+1,ending);
 	}
-	*/
-	float diffvalue = 0;
+
+	return outfilename;
+}
+
+int Normalize (freqresp & values) {
 	// Move to zero range
 	//#define USE_AVERAGE
 	#define USE_MEDIAN
+
+	float diffvalue = 0;
 	#ifdef USE_AVERAGE
 	float average = 0;
 	for (i = 0; i < n; i++){
@@ -120,22 +173,27 @@ main (int argc, char * argv[]){
 	}
 	diffvalue = (median_min+median_max)/2;
 	#endif
+
 	printf("Calculated volume: %.2fdB\n",diffvalue);
 	for (i = 0; i < n; i++){
-		values[i].responsiveness -= diffvalue;
+		values[i].difference = values[i].responsiveness - diffvalue;
 		//printf("%f %f\n",values[i].frequency,values[i].responsiveness);
 	}
 
-	const float foobar_freq[18] = {55.0,77.0,110.0,156.0,220.0,311.0,440.0,622.0,880.0,1200.0,1800.0,2500.0,3500.0,5000.0,7000.0,10000.0,14000.0,20000.0};
-	int foobar_eq[18];
+	return 0;
+}
+
+
+struct foobar_eq GenFoobarEQ () {
+	struct foobar_eq equalizer;
 	// Convert 
 	for (i = 0; i < 18; i++){
 		int low_num=0;
-		while (values[low_num].frequency < foobar_freq[i])
+		while (values[low_num].frequency < equalizer.freq[i])
 			low_num++;
 		if (low_num == 0){
-			printf("Not enough data to equalize %.0fHz\n",foobar_freq[i]);
-			foobar_eq[i] = 0;
+			printf("Not enough data to equalize %.0fHz\n",equalizer.freq[i]);
+			equalizer.value[i] = 0;
 			continue;
 		}
 
@@ -147,26 +205,14 @@ main (int argc, char * argv[]){
 		{
 			float slope = (higher_freq.responsiveness-lower_freq.responsiveness)/(higher_freq.frequency-lower_freq.frequency);
 			//printf("calculated slope: %f\n",slope);
-			float freq_diff = foobar_freq[i]-lower_freq.frequency;
+			float freq_diff = equalizer.freq[i]-lower_freq.frequency;
 			float freq_diff_responsiveness = lower_freq.responsiveness + freq_diff*slope;
 			//printf("calculated responsiveness: %f\n",freq_diff_responsiveness);
 			int rounding = round(freq_diff_responsiveness);
 			//printf("rounded: %d\n",rounding);
-			foobar_eq[i] = 0-rounding;
+			equalizer.value[i] = 0-rounding;
 		}
 	}
 
-	printf("Foobar eq data:\n");
-	FILE * pFileOut;
-	pFileOut = fopen("test.feq","w");
-	if(!pFileOut){
-		printf ("error opening file\n");
-		exit (1);
-	}
-	for (i = 0; i < 18; i++){
-		printf("%.0fHz: %d\n",foobar_freq[i],foobar_eq[i]);
-		fprintf(pFileOut,"%d\n",foobar_eq[i]);
-	}
-	fclose(pFileOut);
-	return 0;
+	return equalizer;
 }
